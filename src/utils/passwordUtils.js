37b13d81655055d75adb7dbd35ed64fe786ab2af
João Pedro LIma <joaopedrocal90@gmail.com>
2025-05-24 04:07:19 -0300

    fix: throwed error to prevent saving undefined passwords

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -2,11 +2,12 @@ import bcrypt from "bcrypt";
 
 const hashPassword = async function (plainPassword) {
   try {
-    const saltRounds = 10
+    const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
     return hashedPassword;
   } catch (error) {
     console.error(`Failed to hash given password, ${error}`);
+    throw error;
   }
 };
 
